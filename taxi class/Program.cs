e7bc6875c161fca0f96771edeb23b2cdb0515e53
HannaOktavianNurHidayat <hannaoktavian@students.amikom.ac.id>
2021-03-18 15:11:23 +0700

    tugastaxi

---
+++ taxi class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // membuat objek dari class Taxi
+            taxi taxi = new taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi class/classtaxi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi_class
+{
+    class taxi
+    {
+        //properties
+        public string Taxi { get; set; }
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        // method 
+        public void TaxiInfo()
+        {
+
+            Console.WriteLine("DriverName: Jono");
+            if (OnDuty) Console.WriteLine("On Duty: Yes");
+            else Console.WriteLine("On Duty : No ");
+            Console.WriteLine("Number Of Passengers : 10");
+            Console.WriteLine();
+        }
+            public void PickUpPassenger()
+            {
+                Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+                Console.WriteLine();
+            }
+            public void DropOffPassenger()
+            {
+                Console.WriteLine("{0} selesai mengatar penumpang", DriverName);
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+
+
+
+   
